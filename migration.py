73082cc05ad59147123d33407535ee05a6b822fc
Moritz Bergmann <mobergmann@pm.me>
2020-09-17 16:35:07 +0200

    WIP: rework of the database (doen't compile)

---
+++ migration.py
@@ -0,0 +1,78 @@
+# ALTER TABLE yeet ADD COLUMN yeet_shield INT NOT NULL DEFAULT 0;
+
+# https://discohook.org/?message=eyJtZXNzYWdlIjp7ImVtYmVkcyI6W3sidGl0bGUiOiJZZWV0IEFjY291bnQiLCJkZXNjcmlwdGlvbiI6IllvdXIgcGVyc29uYWwgWWVldCBwYXNzcG9ydC4gQWxsIHRoZSB5ZWV0IHlvdSBuZWVkISIsInVybCI6Imh0dHBzOi8vZ2l0aHViLmNvbS9tb2JlcmdtYW5uL21yLXllZXQiLCJjb2xvciI6MTQzMjM1NzAsImZpZWxkcyI6W3sibmFtZSI6IuKAjiIsInZhbHVlIjoi4oCOIn0seyJuYW1lIjoiX19ZZWV0IFN0YXRzX18iLCJ2YWx1ZSI6IuKAjiJ9LHsibmFtZSI6IlllZXQgU2NvcmUiLCJ2YWx1ZSI6IjEwLjAxIiwiaW5saW5lIjp0cnVlfSx7Im5hbWUiOiJZZWV0IFJhbmsiLCJ2YWx1ZSI6Il9ZZWV0bGVhZF8iLCJpbmxpbmUiOnRydWV9LHsibmFtZSI6IuKAjiIsInZhbHVlIjoi4oCOIn0seyJuYW1lIjoiVGltZXMgWWVldGV0IiwidmFsdWUiOiIxMiIsImlubGluZSI6dHJ1ZX0seyJuYW1lIjoiVGltZXMgYmVlbiBZZWV0ZXQgIiwidmFsdWUiOiIzMyIsImlubGluZSI6dHJ1ZX0seyJuYW1lIjoi4oCOIiwidmFsdWUiOiLigI4ifSx7Im5hbWUiOiJfX1llZXQgV2FsbGV0X18iLCJ2YWx1ZSI6IuKAjiJ9LHsibmFtZSI6IlllZXQgQ29pbnMiLCJ2YWx1ZSI6IjEwIn0seyJuYW1lIjoiWWVldCBTaGllbGRzIiwidmFsdWUiOiJhY3RpdmUiLCJpbmxpbmUiOnRydWV9LHsibmFtZSI6IlJlbWFpbmluZyIsInZhbHVlIjoiMTAgaCA1NiBtaW4iLCJpbmxpbmUiOnRydWV9XSwiYXV0aG9yIjp7Im5hbWUiOiJEZXJNb3JpdHo5OCIsImljb25fdXJsIjoiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXZhdGFycy8yMTM3MTczOTYwMjY5NDk2MzIvYV9hMjA2MTFhNWY1MzJkNjY5ZWVhODNiNGI0MzI2MmRhZi5naWY_c2l6ZT0xMDI0In19XSwidXNlcm5hbWUiOiJNciBZZWV0IiwiYXZhdGFyX3VybCI6Imh0dHBzOi8vY2RuLmRpc2NvcmRhcHAuY29tL2F2YXRhcnMvNzUzNzE5MjY4MDIxMTA4Nzk2LzQ3MGE0Y2RmYTQyMmIxOWZjZTlhYzQ2ZGY1Mzk1NWFmLnBuZz9zaXplPTEyOCJ9fQ
+# {
+#   "embeds": [
+#     {
+#       "title": "Yeet Account",
+#       "description": "Your personal Yeet passport. All the yeet you need!",
+#       "url": "https://github.com/mobergmann/mr-yeet",
+#       "color": 14323570,
+#       "fields": [
+#         {
+#           "name": "‎",
+#           "value": "‎"
+#         },
+#         {
+#           "name": "__Yeet Stats__",
+#           "value": "‎"
+#         },
+#         {
+#           "name": "Yeet Score",
+#           "value": "10.01",
+#           "inline": true
+#         },
+#         {
+#           "name": "Yeet Rank",
+#           "value": "_Yeetlead_",
+#           "inline": true
+#         },
+#         {
+#           "name": "‎",
+#           "value": "‎"
+#         },
+#         {
+#           "name": "Times Yeetet",
+#           "value": "12",
+#           "inline": true
+#         },
+#         {
+#           "name": "Times been Yeetet ",
+#           "value": "33",
+#           "inline": true
+#         },
+#         {
+#           "name": "‎",
+#           "value": "‎"
+#         },
+#         {
+#           "name": "__Yeet Wallet__",
+#           "value": "‎"
+#         },
+#         {
+#           "name": "Yeet Coins",
+#           "value": "10"
+#         },
+#         {
+#           "name": "Yeet Shields",
+#           "value": "active",
+#           "inline": true
+#         },
+#         {
+#           "name": "Remaining",
+#           "value": "10 h 56 min",
+#           "inline": true
+#         }
+#       ],
+#       "author": {
+#         "name": "DerMoritz98",
+#         "icon_url": "https://cdn.discordapp.com/avatars/213717396026949632/a_a20611a5f532d669eea83b4b43262daf.gif?size=1024"
+#       }
+#     }
+#   ],
+#   "username": "Mr Yeet",
+#   "avatar_url": "https://cdn.discordapp.com/avatars/753719268021108796/470a4cdfa422b19fce9ac46df53955af.png?size=128"
+# }
+
+# Whitespace character
+# '‎'
